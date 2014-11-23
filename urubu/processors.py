9438370b34d9311c832413d05d7c42fe50c44592
Jan Decaluwe <jan.decaluwe@easics.com>
2014-11-23 16:31:17 +0100

    smarty

--- urubu/processors.py
+++ urubu/processors.py
@@ -57,7 +57,7 @@ class ContentProcessor(object):
         tableclass = md_extensions.TableClassExtension() 
         projectref = md_extensions.ProjectReferenceExtension()
         checklist = ChecklistExtension()
-	extensions = ['extra', 'codehilite', 'headerid', 'toc', tableclass, projectref, checklist]
+	extensions = ['extra', 'codehilite', 'headerid', 'toc', 'smarty', tableclass, projectref, checklist]
         extension_configs = { 'codehilite' : [('guess_lang', 'False'),
                                               ('linenums', 'False')],
                               'headerid': [('level', 2)]

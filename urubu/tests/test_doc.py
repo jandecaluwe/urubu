b93aab3b769d13b6ff9b06ca982b2896e00fefdc
Jan Decaluwe <jan@jandecaluwe.com>
2015-11-11 22:58:40 +0100

    Improve site doc test

--- urubu/tests/test_doc.py
+++ urubu/tests/test_doc.py
@@ -1,15 +1,18 @@
 import os
 
-from sh import git 
-from urubu import project
+from sh import git, touch
+from urubu import project, UrubuError
 
 from urubu.tests import cd
 
 def test_doc():
     with cd('../../doc'):
         project.build()
-        d = git('--no-pager', 'diff')
-        print(d)
+        touch('_build/.nojekyll')
+        d = git('--no-pager', 'diff', '--', '_build')
+        if d:
+            print(d)
+            raise UrubuError('Diffs in website')
 
 test_doc()
 

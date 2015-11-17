664cbe7c85ed2c7bc7a1269b39a037c1af582f5c
Jan Decaluwe <jan@jandecaluwe.com>
2015-11-17 21:48:14 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ entry_points = {
 
 setup(
     name="urubu",
-    version="0.7",
+    version="0.8",
     url='http://urubu.jandecaluwe.com',
     author='Jan Decaluwe',
     author_email='jan@jandecaluwe.com',
